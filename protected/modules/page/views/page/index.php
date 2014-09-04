a933217b3d321026fd5ab55b1e08086e97831605
avrocoder <avronchik@gmail.com>
2014-09-04 18:15:01 +0300

    Fixed #13. Fixed breadcrambs in the page on the frontend

--- protected/modules/page/views/page/index.php
+++ protected/modules/page/views/page/index.php
@@ -1,9 +1,10 @@
 <?php
+    $this->breadcrumbs=$this->getBreadCrumbs();
+    
+    $this->pageTitle=Yii::app()->name . " - {$data->title}";
+    $this->pageKeywords = $data->keywords;
+    $this->pageDescription = $data->description;
 
-        $this->pageTitle=Yii::app()->name . " - {$data->title}";
-        $this->pageKeywords = $data->keywords;
-        $this->pageDescription = $data->description;
-
-        echo '<h2 class="page_header">' . $data->title . '</h2>';
-        echo $data->content;
+    echo '<h2 class="page_header">' . $data->title . '</h2>';
+    echo $data->content;
 ?>
