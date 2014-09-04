137cca61e3c4f8e5752ab23054cb78220cbc7d5b
avrocoder <avronchik@gmail.com>
2014-09-04 17:19:08 +0300

    Closed #13. Made support general functional for the breadcrumbs on the frontend side

--- protected/modules/core/components/BaseFrontendController.php
+++ protected/modules/core/components/BaseFrontendController.php
@@ -1,7 +1,10 @@
 <?php
-
 class BaseFrontendController extends BaseController{
-    
-    
+    public function getBreadCrumbs()
+    {
+        return array(
+            'Please, override the method ' . __METHOD__
+        );
+    }
 
 }
