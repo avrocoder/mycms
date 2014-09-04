9071caa97f78f2462248f1f94d35fe2597a5e98a
avrocoder <avronchik@gmail.com>
2014-09-04 17:23:04 +0300

    Closed #14. Made general functional for the upload action on the backend side. This action uses in the editor on the forms

--- protected/modules/page/controllers/PagebackendController.php
+++ protected/modules/page/controllers/PagebackendController.php
@@ -166,18 +166,5 @@ class PagebackendController extends BaseBackendController
 		}
 	}
         
-        public function actionUpload()
-        {
-            $directory = Yii::app()->basePath.'/../public/images/upload/';
-            $file = md5(date('YmdHis')).'.'.pathinfo(@$_FILES['file']['name'], PATHINFO_EXTENSION);
 
-            if (move_uploaded_file(@$_FILES['file']['tmp_name'], $directory.$file)) {
-                 $array = array(
-                                'filelink' => Yii::app()->request->baseUrl.'/public/images/upload/'.$file
-                 );
-            }
-
-            echo CJSON::encode($array);
-            exit ;
-        }
 }
